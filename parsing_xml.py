d0cbc915e7f56fa6ffb09b73c754349e3a60d5f7
ehamayel <eman@ubuntu.asaltech.com>
2018-10-16 03:36:22 -0400

    style: add if __name__ == '__main__'

--- parsing_xml.py
+++ parsing_xml.py
@@ -26,67 +26,65 @@ def valid_ip_socketlib(ip):
     except socket.error:
         return False
 
-#--------------------------------------------------------------------------#
-#                                   Main                                   #
-#--------------------------------------------------------------------------#
-tree = ET.parse("configuration.xml")
-root = tree.getroot()
-dic_iface={}
-for child in root:
-    if child.tag == 'server_ip':
-        server_ip = child.text
-    elif child.tag == 'client_ip':
-        client_ip = child.text
-    elif child.tag == 'interfaces':
-        for sub_child in child:
-            #sub_child.get('name') : server_iface name
-            #sub_child[0].get('name') : client_iface name
-            dic_iface[sub_child.get('name')] = sub_child[0].get('name')
+if __name__ == '__main__':
+    tree = ET.parse("configuration.xml")
+    root = tree.getroot()
+    dic_iface={}
+    for child in root:
+        if child.tag == 'server_ip':
+            server_ip = child.text
+        elif child.tag == 'client_ip':
+            client_ip = child.text
+        elif child.tag == 'interfaces':
+            for sub_child in child:
+                #sub_child.get('name') : server_iface name
+                #sub_child[0].get('name') : client_iface name
+                dic_iface[sub_child.get('name')] = sub_child[0].get('name')
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser(description="Parsing xml arguments")
-ap.add_argument("-s", "--server_ip", help="change server ip value")
-ap.add_argument("-c", "--client_ip", help="change client ip value")
-ap.add_argument("-i", "--iface", nargs=2, help="change client ip value")
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser(description="Parsing xml arguments")
+    ap.add_argument("-s", "--server_ip", help="change server ip value")
+    ap.add_argument("-c", "--client_ip", help="change client ip value")
+    ap.add_argument("-i", "--iface", nargs=2, help="change client ip value")
 
-args = ap.parse_args()       
+    args = ap.parse_args()       
 
-if args.server_ip:
-    new_ip = vars(args)["server_ip"]
-    if(valid_ip(new_ip)):
-        root.find('server_ip').text = new_ip
-        tree.write('configuration.xml')
-        server_ip = new_ip
-    else:
-        print "invalid ip"
+    if args.server_ip:
+        new_ip = vars(args)["server_ip"]
+        if(valid_ip(new_ip)):
+            root.find('server_ip').text = new_ip
+            tree.write('configuration.xml')
+            server_ip = new_ip
+        else:
+            print "invalid ip"
 
 
-if args.client_ip:
-    new_ip = vars(args)["client_ip"]
-    if(valid_ip(new_ip)):
-        root.find('client_ip').text = new_ip
-        tree.write('configuration.xml')
-        client_ip = new_ip
-    else:
-        print "invalid ip"
+    if args.client_ip:
+        new_ip = vars(args)["client_ip"]
+        if(valid_ip(new_ip)):
+            root.find('client_ip').text = new_ip
+            tree.write('configuration.xml')
+            client_ip = new_ip
+        else:
+            print "invalid ip"
 
-if args.iface:
-    key = vars(args)["iface"][0]
-    new_value = vars(args)["iface"][1]
-    exist = False
-    for siface in root.find('interfaces').findall('server_iface'):
-        if siface.get('name') == key:
-            siface[0].set('name', new_value)
-            exist = True
-    if exist:    
-        dic_iface[key] = new_value
-        tree.write('configuration.xml')
-    else:
-        print "not exist server interface" 
+    if args.iface:
+        key = vars(args)["iface"][0]
+        new_value = vars(args)["iface"][1]
+        exist = False
+        for siface in root.find('interfaces').findall('server_iface'):
+            if siface.get('name') == key:
+                siface[0].set('name', new_value)
+                exist = True
+        if exist:    
+            dic_iface[key] = new_value
+            tree.write('configuration.xml')
+        else:
+            print "not exist server interface" 
 
-print "server_ip: ", server_ip
-print "client_ip: ", client_ip
-print "{:<15} {:<15}".format('server_iface','client_iface')
-for item in dic_iface.items():
-    print "{:<15} {:<15}".format(item[0], item[1])
+    print "server_ip: ", server_ip
+    print "client_ip: ", client_ip
+    print "{:<15} {:<15}".format('server_iface','client_iface')
+    for item in dic_iface.items():
+        print "{:<15} {:<15}".format(item[0], item[1])
 
